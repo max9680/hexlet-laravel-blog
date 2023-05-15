fe5d32f889a1d09b958de00de952b675e84a6849
Maxim <max9680@mail.ru>
2023-05-15 21:37:20 +0300

    add route edit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -39,10 +39,28 @@ class ArticleController extends Controller
 
         $article->fill($data);
         $article->save();
-        
-        // $request->session()->flash('success', 'Article was added');
 
         return redirect()
             ->route('articles.index')->with('success', 'Article was added');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('article.edit', compact('article'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $data = $this->validate($request, [
+            'name' => 'required|unique:articles,name,' . $article->id,
+            'body' => 'required|min:100',
+        ]);
+
+        $article->fill($data);
+        $article->save();
+        return redirect()
+            ->route('articles.index');
     }
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2><a href="/articles/{{$article->id}}">{{$article->name}}</a></h2>
+        <h2><a href="/articles/{{$article->id}}">{{$article->name}}</a><p><small><a href="/articles/{{$article->id}}/edit">edit</a></small></p></h2>
         <div>{{Str::limit($article->body, 200)}}</div>
     @endforeach
 @endsection
